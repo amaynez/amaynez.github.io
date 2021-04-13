7327a757f8ea7cca8004f037d5ffd95ed7fdb8b4
amaynez <78620790+amaynez@users.noreply.github.com>
2021-04-13 18:19:49 -0500

    using already created scroll zones

--- assets/js/main.js
+++ assets/js/main.js
@@ -338,8 +338,8 @@
 		if (settings.scrollZones.enabled)
 			(function() {
 
-				var	$left = $('<div class="scrollZone left"></div>'),
-					$right = $('<div class="scrollZone right"></div>'),
+				var	$left = $('<div class="scrollZone left"><svg width:"25px" height:"25px" aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192 512"><path transform="matrix(-1,0,0,-1,192,512)" fill="currentColor" d="M 187.8 264.5 L 41 412.5 c -4.7 4.7 -12.3 4.7 -17 0 L 4.2 392.7 c -4.7 -4.7 -4.7 -12.3 0 -17 L 122.7 256 L 4.2 136.3 c -4.7 -4.7 -4.7 -12.3 0 -17 L 24 99.5 c 4.7 -4.7 12.3 -4.7 17 0 l 146.8 148 c 4.7 4.7 4.7 12.3 0 17 z"></path></svg></div>'),
+					$right = $('<div class="scrollZone right"><svg width:"25px" height:"25px" aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192 512"><path fill="currentColor" d="M187.8 264.5L41 412.5c-4.7 4.7-12.3 4.7-17 0L4.2 392.7c-4.7-4.7-4.7-12.3 0-17L122.7 256 4.2 136.3c-4.7-4.7-4.7-12.3 0-17L24 99.5c4.7-4.7 12.3-4.7 17 0l146.8 148c4.7 4.7 4.7 12.3 0 17z"></path></svg></div>'),
 					$zones = $left.add($right),
 					paused = false,
 					intervalId = null,
